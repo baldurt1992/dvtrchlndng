561f7a765beb9b44e112ac8a3c76ad1f093cc585
baldurt1992 <andresbaldur92@gmail.com>
2024-02-09 08:38:06 -0500

    button contact edit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e5ba6ca39efd5b57"; }
+/******/ 		__webpack_require__.h = function() { return "9e44cd9671f78f85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/ContactButton/ContactButton.tsx
+++ src/components/ContactButton/ContactButton.tsx
@@ -1,31 +1,22 @@
 "use client";
-import Link from "next/link";
-import { useState } from "react";
 
 export function ContactButton() {
-  const [clic, setclic] = useState(false);
-
-  const handleClick = () => {
-    setclic(true);
+  const handleRedirect = () => {
+    window.location.href = "https://api.whatsapp.com/send/?phone=3117261670";
   };
 
   return (
     <div className="">
-      <Link
-        href="https://api.whatsapp.com/send/?phone=3117261670"
-        className="flex flex-auto"
-      >
-        <div className="btn-bg bg-2 flex jusfify-center items-center justify-center items-center">
-          <div className="flex flex-row items-center justify-center px-0 py-2 button-contact btn btn-2 md:px-0">
-            <button
-              onClick={handleClick}
-              className="text-sl md:text-lg shadow-button"
-            >
-              Contáctanos
-            </button>
-          </div>
+      <div className="btn-bg bg-2 flex justify-center items-center">
+        <div className="flex flex-row items-center justify-center px-4 py-2 button-contact btn btn-2">
+          <button
+            onClick={handleRedirect}
+            className="text-sl md:text-lg shadow-button"
+          >
+            Contáctanos
+          </button>
         </div>
-      </Link>
+      </div>
     </div>
   );
 }
