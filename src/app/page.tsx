8157955b9a4d1ae18b689bb05a5c56cd4fe318a4
baldurt1992 <andresbaldur92@gmail.com>
2024-02-08 19:12:01 -0500

    extension

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { BestBusiness } from "@/components/Your-Imagine/BestBusiness";
-import { Services } from "@/components/Services/CounterData";
-import { FirstBlock } from "@/components/FirstBlock/FirstBlock";
-import { Footer } from "@/components/Footer/Footer";
-import { PaymentsMethods } from "@/components/Space/Space";
-import { Header } from "@/components/Header/Header";
-import { Contact } from "@/components/Contact/Contact";
+import { BestBusiness } from "@/components/Your-Imagine/BestBusiness.tsx";
+import { Services } from "@/components/Services/CounterData.jsx";
+import { FirstBlock } from "@/components/FirstBlock/FirstBlock.tsx";
+import { Footer } from "@/components/Footer/Footer.tsx";
+import { PaymentsMethods } from "@/components/Space/Space.tsx";
+import { Header } from "@/components/Header/Header.jsx";
+import { Contact } from "@/components/Contact/Contact.tsx";
 
 export default function Home() {
   return (
